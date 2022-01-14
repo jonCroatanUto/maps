8afa8a21d180bb99f374ea7640435f474d84892e
JonCroatan <43881219+jonCroatanUto@users.noreply.github.com>
2022-01-14 18:36:05 +0100

    add a map with google maps api in Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,18 @@
 import React from "react";
+import "./home.css";
+import { Loader } from "@googlemaps/js-api-loader";
 function Home() {
-  return <div>I'm home</div>;
+  let map: google.maps.Map;
+  const loader = new Loader({
+    apiKey: "AIzaSyBf9nU8O65mgqqR2jACgDn03BMLHY7q_Ak",
+    version: "weekly",
+  });
+  loader.load().then(() => {
+    map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+      center: { lat: -34.397, lng: 150.644 },
+      zoom: 8,
+    });
+  });
+  return <div id="map"></div>;
 }
 export default Home;
