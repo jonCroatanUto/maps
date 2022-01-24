4bd2b43970290cda9fb105ecc6f48ab3a443e314
JonCroatan <43881219+jonCroatanUto@users.noreply.github.com>
2022-01-24 13:42:36 +0100

    ajustar estils, netejar codi

--- src/components/Maps/Map.tsx
+++ src/components/Maps/Map.tsx
@@ -1,23 +1,14 @@
 import React, { useState, useEffect } from "react";
 
-// import { Loader } from "@googlemaps/js-api-loader";
 import ReactGoogleMapLoader from "react-google-maps-loader";
 import ReactGooglePlacesSuggest from "react-google-places-suggest";
 import Input from "../Input";
 import GoogleMapReact from "google-map-react";
 import CurrentMarker from "../currentMarker";
 
-// import ReactGoogleMap from "react-google-map";
 function Map() {
-  // const [currentMarkerCorrdenates, setCurrentMarkerCorrdenates] = useState({
-  //   lat: null,
-  //   lng: null,
-  // });
-
   const [markerListCorrdenates, setMarkerListCorrdenates] = useState<any>([]);
 
-  // const { lat, lng } = currentMarkerCorrdenates;
-  //const [predictionLine, setPredictionLine] = useState();
   const [zoom, setZoom] = useState(4);
   const [state, setState] = useState({
     search: "",
@@ -31,16 +22,11 @@ function Map() {
     geocodedPrediction: any,
     originalPrediction: any
   ) {
-    // if (lat !== null && lng !== null) {
     markerListCorrdenates.push({
       lat: geocodedPrediction.geometry.viewport.Ab.g,
       lng: geocodedPrediction.geometry.viewport.Ra.g,
     });
 
-    // setCurrentMarkerCorrdenates({
-    //   lat: geocodedPrediction.geometry.viewport.Ab.h,
-    //   lng: geocodedPrediction.geometry.viewport.Ra.h,
-    // }); // eslint-disable-line
     setState({
       search: "",
       value: geocodedPrediction.formatted_address,
@@ -62,11 +48,7 @@ function Map() {
                 googleMaps={googleMaps}
                 autocompletionRequest={{
                   input: state.search,
-                  // Optional options
-                  // https://developers.google.com/maps/documentation/javascript/reference?hl=fr#AutocompletionRequest
                 }}
-                // Optional props
-
                 onSelectSuggest={handleSelectSuggest}
                 textNoResults="My custom no results text" // null or "" if you want to disable the no results item
                 customRender={(prediction) => (
@@ -104,7 +86,7 @@ function Map() {
                   handleChange={handleInputChange}
                 />
               </ReactGooglePlacesSuggest>
-              <div style={{ height: "100vh", width: "100%" }}>
+              <div style={{ height: "91vh", width: "100%" }}>
                 <GoogleMapReact
                   bootstrapURLKeys={{
                     key: "AIzaSyBf9nU8O65mgqqR2jACgDn03BMLHY7q_Ak",
@@ -115,12 +97,6 @@ function Map() {
                   }}
                   defaultZoom={zoom}
                 >
-                  {/* {lat != null && lng !== null ? (
-                    <CurrentMarker lat={lat} lng={lng} text="My Marker" />
-                  ) : (
-                    <div></div>
-                  )} */}
-
                   {markerListCorrdenates.map((marker: any) => {
                     return (
                       <CurrentMarker
--- src/components/oldMarkers/OldMarkers.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-function OldMarkers() {
-  return <div></div>;
-}
-export default OldMarkers;
